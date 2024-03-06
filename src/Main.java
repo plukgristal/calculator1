e7b2783d4493baf7bd68d97c90e44d7b84656eea
plugristal <silaby84@gmail.com>
2024-03-06 17:41:24 +0100

    Calculator

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,44 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
+        Scanner scanner = new Scanner(System.in);
+        try {
+
+            String snum1 = scanner.next();
+            String snum2 = scanner.next();
+            String type = scanner.next();
+
+            int num1 = Integer.parseInt(snum1);
+            int num2 = Integer.parseInt(snum2);
+
+            int result;
+            switch(type) {
+                case "sum":
+                    result = num1 + num2;
+                    break;
+                case "sub":
+                    result = num1 - num2;
+                    break;
+                case "mul":
+                    result = num1 * num2;
+                    break;
+                case "div":
+                    if (num2 == 0) {
+                        System.out.println("Cannot divide by zero.");
+                        return;
+                    }
+                    result = num1 / num2;
+                    break;
+                default:
+                    System.out.println("Operation not found! Use one of: sum, sub, mul, div.");
+                    return;
+            }
+            System.out.println("Result: " + result);
+        } catch (NumberFormatException e) {
+            System.out.println("Error: Both arguments must be integers.");
+        } catch (Exception e) {
+            System.out.println("An unexpected error occurred: " + e.getMessage());
         }
     }
 }
